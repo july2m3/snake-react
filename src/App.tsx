41ff450c314bd20b24b52189e1ab21ca6f63d970
july2m3 <july2m3@gmail.com>
2020-08-04 15:52:43 -0500

    16 x 16 grid created fill with all 0s

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+// import constants
 
-const App = () => {
+const App: React.SFC = () => {
+  const [grid, updateGrid] = useState<number[][] | null>();
+
+  //setup
+  useEffect(() => {
+    // create 16 x 16 2d array filled with 0s
+    const tempGrid = new Array(16).fill(0).map(() => new Array(16).fill(0));
+
+    updateGrid(tempGrid);
+  }, []);
+
   return (
     <>
       <h1>Hello world</h1>
