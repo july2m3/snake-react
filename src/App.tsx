efa039b20eca1ffe98cdd6607e0026244100f2cb
Julio <july2m3@gmail.com>
2020-08-06 21:37:19 -0500

    Added keyboard control

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+/* eslint-disable no-console */
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
 import React, { useState, useEffect } from 'react';
 
 import './main.scss';
@@ -5,32 +9,73 @@ import './main.scss';
 import Map from './components/map';
 
 // import constants
-import { FOOD, PLAYER } from './components/constants';
+import {
+  PLAYER,
+  LEFT_KEY,
+  RIGHT_KEY,
+  UP_KEY,
+  DOWN_KEY,
+} from './components/constants';
 
 const App: React.SFC = () => {
   const [grid, updateGrid] = useState<number[][] | null | undefined>();
+  const [emptyGrid, updateEmptyGrid] = useState<
+    number[][] | null | undefined
+  >();
   const [player, updatePlayer] = useState<Object | null>();
+  const [apple, updateApple] = useState<Object | null>();
 
-  const putPlayerInGrid = () => {
+  function putPlayerInGrid() {
     const tempGrid = [grid];
-    console.log(tempGrid);
-  };
+    console.log({ tempGrid });
+  }
 
-  // setup
+  function movePlayer(upOrDown = 0, leftOrRight = 0) {
+    // const tempGrid = [grid];
+    console.log('moving snake', { upOrDown, leftOrRight });
+  }
+
+  function getNewApplePosition() {}
+
+  function updateApplePosition() {}
+
+  function handleKeyDownEvent(event: any) {
+    switch (event.keyCode) {
+      case UP_KEY:
+        movePlayer(-1, 0);
+        break;
+      case RIGHT_KEY:
+        movePlayer(0, 1);
+        break;
+      case DOWN_KEY:
+        movePlayer(1, 0);
+        break;
+      case LEFT_KEY:
+        movePlayer(0, -1);
+        break;
+      default:
+        break;
+    }
+  }
+
+  // setup grid, player and apple, and keyboard
   useEffect(() => {
-    //  create 16 x 16 2d array filled with 0s
     const tempGrid = new Array(16).fill(0).map(() => new Array(16).fill(0));
 
     const tempPlayer = {
-      positionRow: 8,
-      positionColumn: 8,
-      size: 1,
+      coorindates: [{ row: 8, col: 8 }],
+      head: { row: 8, col: 8 },
     };
 
-    tempGrid[8][8] = PLAYER;
-
-    updatePlayer(tempPlayer);
+    updateEmptyGrid(tempGrid);
     updateGrid(tempGrid);
+    updatePlayer(tempPlayer);
+    document.addEventListener('keydown', handleKeyDownEvent);
+
+    //  wait for stuff to load a lil
+    setTimeout(() => {
+      putPlayerInGrid();
+    }, 100);
   }, []);
 
   return <>{grid && <Map grid={grid} />}</>;
--- src/components/constants.ts
+++ src/components/constants.ts
@@ -1,4 +1,8 @@
 const FOOD = 1;
 const PLAYER = 2;
+const LEFT_KEY = 37;
+const UP_KEY = 38;
+const RIGHT_KEY = 39;
+const DOWN_KEY = 40;
 
-export { FOOD, PLAYER };
+export { FOOD, PLAYER, LEFT_KEY, UP_KEY, RIGHT_KEY, DOWN_KEY };
