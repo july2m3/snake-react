93bb16884bb4c4ac6559f20b596553fa4929d96c
Julio <july2m3@gmail.com>
2020-08-06 22:05:43 -0500

    Fixed eslint errors

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,7 +1,12 @@
-import React from "react";
-import { v4 as uuidV4 } from "uuid";
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-plusplus */
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import { v4 as uuidV4 } from 'uuid';
 
-import { FOOD, PLAYER } from "../constants";
+import { FOOD, PLAYER } from '../constants';
 
 interface mapProps {
   grid: number[][];
@@ -10,31 +15,34 @@ interface mapProps {
 const Map: React.SFC<mapProps> = (props: mapProps) => {
   const { grid } = props;
 
-  const renderedGrid = new Array();
+  const renderedGrid = [];
 
   const getClassName = (j: number, i: number) => {
     // check for worm, or apple
     if (grid[i][j] === PLAYER) {
-      return "player";
-    } else if (grid[i][j] === FOOD) {
-      return "food";
+      return 'player';
     }
-
+    if (grid[i][j] === FOOD) {
+      return 'food';
+    }
     if (j % 2 === 0) {
-      return i % 2 === 0 ? "box-one" : "box-two";
-    } else {
-      return i % 2 === 0 ? "box-two" : "box-one";
+      return i % 2 === 0 ? 'box-one' : 'box-two';
     }
+    return i % 2 === 0 ? 'box-two' : 'box-one';
   };
 
   for (let j = 0; j < grid.length; j++) {
     for (let i = 0; i < grid.length; i++) {
-      let className = getClassName(j, i);
-      renderedGrid.push(<div className={className} key={uuidV4()}></div>);
+      const className = getClassName(j, i);
+      renderedGrid.push(<div className={className} key={uuidV4()} />);
     }
   }
 
-  return <div className="map">{renderedGrid}</div>;
+  return (
+    <div className="map" key={grid.toString()}>
+      {renderedGrid}
+    </div>
+  );
 };
 
 export default Map;
