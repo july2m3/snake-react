4d59eb357d769bf4e8095ad19c19e79641c769e3
Julio <july2m3@gmail.com>
2020-08-06 21:58:52 -0500

    Added update functions for game pieces

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,17 @@ import Map from './components/map';
 // import constants
 import {
   PLAYER,
+  FOOD,
   LEFT_KEY,
   RIGHT_KEY,
   UP_KEY,
   DOWN_KEY,
 } from './components/constants';
+
+interface Apple {
+  column: number;
+  row: number;
+}
 
 const App: React.SFC = () => {
   const [grid, updateGrid] = useState<number[][] | null | undefined>();
@@ -23,21 +29,22 @@ const App: React.SFC = () => {
     number[][] | null | undefined
   >();
   const [player, updatePlayer] = useState<Object | null>();
-  const [apple, updateApple] = useState<Object | null>();
-
-  function putPlayerInGrid() {
-    const tempGrid = [grid];
-    console.log({ tempGrid });
-  }
+  const [apple, updateApple] = useState<Apple | null>();
 
   function movePlayer(upOrDown = 0, leftOrRight = 0) {
     // const tempGrid = [grid];
     console.log('moving snake', { upOrDown, leftOrRight });
   }
 
-  function getNewApplePosition() {}
+  function getNewApplePosition() {
+    const row = Math.floor(Math.random() * 16);
+    const column = Math.floor(Math.random() * 16);
+    return { row, column };
+  }
 
-  function updateApplePosition() {}
+  function updateApplePosition() {
+    updateApple(getNewApplePosition());
+  }
 
   function handleKeyDownEvent(event: any) {
     switch (event.keyCode) {
@@ -58,6 +65,22 @@ const App: React.SFC = () => {
     }
   }
 
+  // update Grid by putting in current player position and apple position
+  function updateGameBoard() {
+    if (
+      emptyGrid === undefined ||
+      player === undefined ||
+      apple === undefined
+    ) {
+      return;
+    }
+
+    const tempGrid = emptyGrid;
+    tempGrid![apple!.row][apple!.column] = PLAYER;
+    updateGrid(grid);
+    console.log(tempGrid, { apple });
+  }
+
   // setup grid, player and apple, and keyboard
   useEffect(() => {
     const tempGrid = new Array(16).fill(0).map(() => new Array(16).fill(0));
@@ -70,13 +93,13 @@ const App: React.SFC = () => {
     updateEmptyGrid(tempGrid);
     updateGrid(tempGrid);
     updatePlayer(tempPlayer);
+    updateApplePosition();
     document.addEventListener('keydown', handleKeyDownEvent);
-
-    //  wait for stuff to load a lil
-    setTimeout(() => {
-      putPlayerInGrid();
-    }, 100);
   }, []);
+
+  useEffect(() => {
+    updateGameBoard();
+  }, [player, apple]);
 
   return <>{grid && <Map grid={grid} />}</>;
 };
